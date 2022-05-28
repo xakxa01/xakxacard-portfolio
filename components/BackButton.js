ea5d1f195d794d46cd16817a224356a20d9192f6
xakxa <Carlosxaviergomez@outlook.com>
2022-05-27 22:29:04 -0400

    mejora de posicionamiento

--- components/BackButton.js
+++ components/BackButton.js
@@ -4,16 +4,16 @@ import styles from "../styles/backButton.module.scss"
 
 export default function BackButton() {
 	return (
-		<div className={styles.backButtonContainer}>
-			<div
-				className={styles.backButton}
-			>
-				<Image
-					src="https://img.icons8.com/material-rounded/30/0099ff/restart--v1.png"
-					alt="back button"
-					width={30}
-					height={30}
-				/>
+		<div className={styles.backButtonPosition}>
+			<div className={styles.backButtonContainer}>
+				<div className={styles.backButton} >
+					<Image
+						src="https://img.icons8.com/material-rounded/30/0099ff/restart--v1.png"
+						alt="back button"
+						width={30}
+						height={30}
+					/>
+				</div>
 			</div>
 		</div>
 	)
