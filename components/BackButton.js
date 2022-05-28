6b5cf1727aee06d4509d1ff526d837e54209e5db
xakxa <Carlosxaviergomez@outlook.com>
2022-05-27 23:29:18 -0400

    retoques para el responsive

--- components/BackButton.js
+++ components/BackButton.js
@@ -8,10 +8,10 @@ export default function BackButton() {
 			<div className={styles.backButtonContainer}>
 				<div className={styles.backButton} >
 					<Image
-						src="https://img.icons8.com/material-rounded/30/0099ff/restart--v1.png"
+						src="https://img.icons8.com/material-rounded/35/0099ff/restart--v1.png"
 						alt="back button"
-						width={30}
-						height={30}
+						width={35}
+						height={35}
 					/>
 				</div>
 			</div>
