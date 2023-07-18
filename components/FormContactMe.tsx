64e2f3774fc6b78bfc06b39d5812163dba0b7275
xakxa01 <Carlosxaviergomez@outlook.com>
2023-07-18 07:36:30 -0400

    añadiendo type a form

--- components/FormContactMe.tsx
+++ components/FormContactMe.tsx
@@ -3,22 +3,33 @@ import emailJS from "@emailjs/browser";
 import { Formik, Form, Field } from "formik";
 import toast, { Toaster } from "react-hot-toast";
 
+type Tvalues = {
+  email: string;
+  message: string;
+};
+
 const FormContactMe = () => {
   const initialValues = {
     email: "",
     message: "",
   };
 
-  const handlerSubmit = async (values, { resetForm }) => {
+  const handlerSubmit = async (
+    values: Tvalues,
+    { resetForm }: { resetForm: () => void }
+  ) => {
     await emailJS
       .send(
-        process.env.NEXT_PUBLIC_TEMPLATE_ID,
-        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID as string,
+        process.env.NEXT_PUBLIC_SERVICE_ID as string,
         values,
         process.env.NEXT_PUBLIC_PUBLIC_KEY
       )
       .then(() => toast.success("email sended"))
-      .catch(() => toast.error("error sending email"));
+      .catch((err) => {
+        console.log("err:", err);
+        toast.error("error sending email");
+      });
 
     resetForm();
   };
